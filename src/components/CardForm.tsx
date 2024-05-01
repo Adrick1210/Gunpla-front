11c8b7bebdd7a464cd60e47be5515cef963a1fc9
Christian Alexander <christian.alexanderato@gmail.com>
2024-04-30 18:07:08 -0700

    new form updates

--- src/components/CardForm.tsx
+++ src/components/CardForm.tsx
@@ -1,5 +1,6 @@
+import { useState } from "react";
 import { useFormik } from "formik";
-import { TextField, Grid, Typography } from "@mui/material";
+import { TextField, Grid } from "@mui/material";
 import * as Yup from "yup";
 
 interface CardInformation {
@@ -15,6 +16,12 @@ const validatedSchema = Yup.object().shape({
 });
 
 const CardForm: React.FC = () => {
+  const [placeholders, setPlaceholders] = useState({
+    cardNumber: "0000-0000-0000-0000",
+    expirationDate: "MM/YY",
+    cvv: "000",
+  });
+
   const formik = useFormik<CardInformation>({
     initialValues: {
       cardNumber: "",
@@ -27,55 +34,82 @@ const CardForm: React.FC = () => {
     },
   });
 
+  const handleCardNumberInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const formattedValue = e.target.value
+      .replace(/\D/g, "")
+      .replace(/(\d{4})(?=\d)/g, "$1-"); // Add dashes every 4 characters
+    formik.setFieldValue("cardNumber", formattedValue);
+    setPlaceholders((prevPlaceholders) => ({
+      ...prevPlaceholders,
+      cardNumber: formattedValue ? formattedValue : "0000-0000-0000-0000",
+    }));
+  };
+
+  const handleExpirationDateInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const formattedValue = e.target.value
+      .replace(/\D/g, "")
+      .replace(/(\d{2})(?=\d)/g, "$1/"); // Add slash every 2 characters
+    formik.setFieldValue("expirationDate", formattedValue);
+    setPlaceholders((prevPlaceholders) => ({
+      ...prevPlaceholders,
+      expirationDate: formattedValue ? formattedValue : "MM/YY",
+    }));
+  };
+
+  const handleCVVInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const formattedValue = e.target.value.replace(/\D/g, "");
+    formik.setFieldValue("cvv", formattedValue);
+    setPlaceholders((prevPlaceholders) => ({
+      ...prevPlaceholders,
+      cvv: formattedValue ? formattedValue : "000",
+    }));
+  };
+
   return (
     <div>
       <h3>Card Information</h3>
-      <form onSubmit={formik.handleSubmit}>
+      <form>
         <Grid container spacing={2}>
           <Grid item xs={12}>
             <TextField
-              sx={{width: "650px"}}
+              sx={{ width: "650px" }}
               id="cardNumber"
               name="cardNumber"
               label="Card Number"
               variant="outlined"
               value={formik.values.cardNumber}
-              onChange={formik.handleChange}
-              error={
-                formik.touched.cardNumber && Boolean(formik.errors.cardNumber)
-              }
+              onChange={handleCardNumberInput}
+              error={formik.touched.cardNumber && Boolean(formik.errors.cardNumber)}
               helperText={formik.touched.cardNumber && formik.errors.cardNumber}
+              placeholder={placeholders.cardNumber}
             />
           </Grid>
           <Grid item xs={4}>
             <TextField
-              sx={{width: "300px", marginLeft: "250px"}}
+              sx={{ width: "300px", marginLeft: "250px" }}
               id="expirationDate"
               name="expirationDate"
               label="Expiration Date"
               variant="outlined"
               value={formik.values.expirationDate}
-              onChange={formik.handleChange}
-              error={
-                formik.touched.expirationDate &&
-                Boolean(formik.errors.expirationDate)
-              }
-              helperText={
-                formik.touched.expirationDate && formik.errors.expirationDate
-              }
+              onChange={handleExpirationDateInput}
+              error={formik.touched.expirationDate && Boolean(formik.errors.expirationDate)}
+              helperText={formik.touched.expirationDate && formik.errors.expirationDate}
+              placeholder={placeholders.expirationDate}
             />
           </Grid>
           <Grid item xs={1}>
             <TextField
-              sx={{width: "100px", marginLeft: "250px"}}
+              sx={{ width: "100px", marginLeft: "250px" }}
               id="cvv"
               name="cvv"
               label="CVV"
               variant="outlined"
               value={formik.values.cvv}
-              onChange={formik.handleChange}
+              onChange={handleCVVInput}
               error={formik.touched.cvv && Boolean(formik.errors.cvv)}
               helperText={formik.touched.cvv && formik.errors.cvv}
+              placeholder={placeholders.cvv}
             />
           </Grid>
         </Grid>
