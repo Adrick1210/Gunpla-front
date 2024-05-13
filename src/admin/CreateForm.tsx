79526005251d5fd7141ea53913a112f968bd0103
Christian Alexander <christian.alexanderato@gmail.com>
2024-05-12 18:56:27 -0700

    forms for create and update added

--- src/admin/CreateForm.tsx
+++ src/admin/CreateForm.tsx
@@ -1,40 +1,88 @@
 import React from "react";
-import { TextField, Paper } from "@mui/material";
+import { TextField, Paper, Button, Box } from "@mui/material";
 import AdminNav from "./AdminNav";
 
 function CreateForm() {
-
   return (
     <div>
       <AdminNav />
       <h1>Add to Inventory </h1>
-      <Paper elevation={3} className="form-paper">
-        <TextField sx={{ width: "500px" }} id="name" name="name" label="Name" />
-      </Paper>
-      <Paper elevation={3} className="form-paper">
-        <TextField
-          sx={{ width: "500px" }}
-          id="brand"
-          name="brand"
-          label="Brand"
-        />
-      </Paper>
-      <Paper elevation={3} className="form-paper">
-        <TextField
-          sx={{ width: "500px" }}
-          id="boxArt"
-          name="boxArt"
-          label="Box Art URL"
-        />
-      </Paper>
-      <Paper elevation={3} className="form-paper">
-        <TextField
-          sx={{ width: "500px" }}
-          id="address"
-          name="address"
-          label="Address"
-        />
-      </Paper>
+      <div className="word-input">
+        <Paper elevation={3} className="form-paper">
+          <TextField
+            sx={{ width: "500px" }}
+            id="name"
+            name="name"
+            label="Name"
+          />
+        </Paper>
+        <Paper elevation={3} className="form-paper">
+          <TextField
+            sx={{ width: "500px" }}
+            id="brand"
+            name="brand"
+            label="Brand"
+          />
+        </Paper>
+        <Paper elevation={3} className="form-paper">
+          <TextField
+            sx={{ width: "500px" }}
+            id="boxArt"
+            name="boxArt"
+            label="Box Art URL"
+          />
+        </Paper>
+        <Paper elevation={3} className="form-paper">
+          <TextField
+            sx={{ width: "500px" }}
+            id="price"
+            name="price"
+            label="Price"
+          />
+        </Paper>
+        <Paper elevation={3} className="form-paper">
+          <TextField
+            sx={{ width: "500px" }}
+            id="inventory"
+            name="inventory"
+            label="Inventory"
+          />
+        </Paper>
+        <Paper elevation={3} className="form-paper">
+          <TextField
+            sx={{ width: "500px" }}
+            id="description"
+            name="description"
+            label="Description"
+          />
+        </Paper>
+        <Paper elevation={3} className="form-paper">
+          <TextField
+            sx={{ width: "500px" }}
+            id="grade"
+            name="grade"
+            label="Grade"
+          />
+        </Paper>
+        <Paper elevation={3} className="form-paper">
+          <TextField
+            sx={{ width: "500px" }}
+            id="scale"
+            name="scale"
+            label="Scale"
+          />
+        </Paper>
+        <Box>
+          <Button
+            sx={{ marginTop: "20px", marginBottom: "20px" }}
+            color="success"
+            variant="contained"
+            type="submit"
+          >
+            Add Product
+          </Button>
+        </Box>
+      </div>
     </div>
   );
 }
--- src/admin/UpdateForm.tsx
+++ src/admin/UpdateForm.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { TextField, Paper, Button, Box } from "@mui/material";
 import AdminNav from './AdminNav';
 
  function UpdateForm() {
@@ -6,6 +7,82 @@ import AdminNav from './AdminNav';
     <div>
       <AdminNav />
       <h1>Edit Form</h1>
+      <div className="word-input">
+        <Paper elevation={3} className="form-paper">
+          <TextField
+            sx={{ width: "500px" }}
+            id="name"
+            name="name"
+            label="Name"
+          />
+        </Paper>
+        <Paper elevation={3} className="form-paper">
+          <TextField
+            sx={{ width: "500px" }}
+            id="brand"
+            name="brand"
+            label="Brand"
+          />
+        </Paper>
+        <Paper elevation={3} className="form-paper">
+          <TextField
+            sx={{ width: "500px" }}
+            id="boxArt"
+            name="boxArt"
+            label="Box Art URL"
+          />
+        </Paper>
+        <Paper elevation={3} className="form-paper">
+          <TextField
+            sx={{ width: "500px" }}
+            id="price"
+            name="price"
+            label="Price"
+          />
+        </Paper>
+        <Paper elevation={3} className="form-paper">
+          <TextField
+            sx={{ width: "500px" }}
+            id="inventory"
+            name="inventory"
+            label="Inventory"
+          />
+        </Paper>
+        <Paper elevation={3} className="form-paper">
+          <TextField
+            sx={{ width: "500px" }}
+            id="description"
+            name="description"
+            label="Description"
+          />
+        </Paper>
+        <Paper elevation={3} className="form-paper">
+          <TextField
+            sx={{ width: "500px" }}
+            id="grade"
+            name="grade"
+            label="Grade"
+          />
+        </Paper>
+        <Paper elevation={3} className="form-paper">
+          <TextField
+            sx={{ width: "500px" }}
+            id="scale"
+            name="scale"
+            label="Scale"
+          />
+        </Paper>
+        <Box>
+          <Button
+            sx={{ marginTop: "20px", marginBottom: "20px" }}
+            color="success"
+            variant="contained"
+            type="submit"
+          >
+            Update Product
+          </Button>
+        </Box>
+      </div>
     </div>
   )
 }
